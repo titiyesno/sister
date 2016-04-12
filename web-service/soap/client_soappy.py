5ad9d852c141ebee8e295336b401030305679eb0
titiyesno <titiesovari@gmail.com>
2016-04-12 18:41:26 +0700

    dari 1 server oke

--- web-service/soap/client_soappy.py
+++ web-service/soap/client_soappy.py
@@ -1,3 +1,21 @@
 import SOAPpy
+import pickle
+
 server = SOAPpy.SOAPProxy("http://localhost:5000/")
-print server.hello()
+start = server.hello()
+logevent = {}
+isi = []
+isi = pickle.loads(start)
+#print len(isi)
+for i in isi:
+        #print i
+    split = i.split()
+    event = split[3] + " " + split[4]
+    #print event
+    if event not in logevent:
+        logevent[event] = 1
+    else:
+        logevent[event] += 1
+
+for event in logevent:
+	print event, logevent[event]
--- web-service/soap/server_soappy.py
+++ web-service/soap/server_soappy.py
@@ -1,6 +1,29 @@
 import SOAPpy
+import pickle
+import os.path
+
 def hello():
-    return "Hello World"
+	path = '/home/tities/var/log/cups/'
+	num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.startswith('error_log')])
+	nama_file = ""
+	iter = 0
+	isi = []
+	while (iter < num_files) :
+		
+		if iter == 0 :
+			nama_file = path + "error_log"
+		else :
+			nama_file = path + "error_log." + str(iter)
+		
+		with open(nama_file,'r') as f:
+			for line in f:
+				message = line.strip('\n')
+				isi.append(message)
+			
+		f.close()
+		#print len(isi)
+		iter=iter+1
+	return pickle.dumps(isi)
 server = SOAPpy.SOAPServer(("localhost", 5000))
 server.registerFunction(hello)
 server.serve_forever()
