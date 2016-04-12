89b8f5daee3480e4b4189c5f34a4dec2b0d5603f
titiyesno <titiesovari@gmail.com>
2016-04-12 20:21:05 +0700

    sudah banyak server, tinggal dites :)

--- web-service/soap/client_soappy.py
+++ web-service/soap/client_soappy.py
@@ -1,21 +1,39 @@
 import SOAPpy
 import pickle
 
-server = SOAPpy.SOAPProxy("http://localhost:5000/")
-start = server.hello()
 logevent = {}
 isi = []
-isi = pickle.loads(start)
+final = []
+
+with open('ip.txt','r') as bukafile:
+	ipini = str(bukafile.read()).split('\n')
+	#print ipini
+	panjang = len(ipini)
+	#print panjang
+bukafile.close()
+
+iter = 0
+while(iter < panjang-1):
+	server = SOAPpy.SOAPProxy("http://localhost:"+str(ipini[iter])+"/")
+	start = server.hello()
+	isi = pickle.loads(start)
+	for i in isi:
+		final.append(i)
+	#cek = "http://localhost:"+str(ipini[iter])+"/"
+	#print cek
+	#print isi
+	iter += 1
+
+#print final
 #print len(isi)
-for i in isi:
-        #print i
-    split = i.split()
-    event = split[3] + " " + split[4]
-    #print event
-    if event not in logevent:
-        logevent[event] = 1
-    else:
-        logevent[event] += 1
+for t in final:
+	#print t
+	split = t.split()
+	event = split[3] + " " + split[4]
+	if event not in logevent:
+		logevent[event] = 1
+	else:
+		logevent[event] += 1
 
 for event in logevent:
 	print event, logevent[event]
